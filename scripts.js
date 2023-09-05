1ab872cc8fe2fb6e975783e17bf6bce0c57826f6
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-05 20:08:48 +0200

    added toggle icon at dark/light mode

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ colorMode.addEventListener('click', () => {
     isDarkMode = !isDarkMode
     if(document.body.classList.contains('light')){
         document.body.classList = 'dark';
+        colorMode.innerHTML = '<i class="fa-regular fa-sun fa-xl" style="color: #000000;"></i>'
     }else{
         document.body.classList = 'light';
+        colorMode.innerHTML = '<i class="fa-solid fa-moon fa-2xl" style="color: #000000;"></i>';
     }
 })
