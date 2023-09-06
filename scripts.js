dfe69202cb53304741fb60c2a4bd9f5c348ab48b
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-06 11:31:21 +0200

    timer clock working correctly

--- scripts.js
+++ scripts.js
@@ -1,29 +1,44 @@
 let isDarkMode = false;
-const date = new Date()
-const day = date.getDate();
-const month = date.getMonth() + 1;
-let minute = date.getMinutes();
-let hour = date.getHours();
-
 
 const colorMode = document.querySelector('#toggle-mode');
 const hoursContainer = document.querySelector('#hour');
 const dateContainer = document.querySelector('#date');
 
 const updateDateHour = () => {
+    const date = new Date()
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    let minute = date.getMinutes();
+    let hour = date.getHours();
+    let second = date.getSeconds();
+
+    const secondsHand = document.querySelector('#seconds');
+    const minutesHand = document.querySelector('#minutes');
+    const hoursHand = document.querySelector('#hours');
+
     hoursContainer.textContent = `${hour}:${minute}`
     dateContainer.textContent = `${day}, ${month}`
+    secondsHand.style.transform = `rotate(${second * 6}deg)`;
+    minutesHand.style.transform = `rotate(${minute * 6}deg)`;
+    hoursHand.style.transform = `rotate(${hour * 30}deg)`;
 }
 
 colorMode.addEventListener('click', () => {
+    const minutesHand = document.querySelector('#minutes');
+    const hoursHand = document.querySelector('#hours');
     isDarkMode = !isDarkMode
     if(document.body.classList.contains('light')){
         document.body.classList = 'dark';
-        colorMode.innerHTML = '<i class="fa-regular fa-sun fa-xl" style="color: #000000;"></i>'
+        colorMode.innerHTML = '<i class="fa-regular fa-sun fa-xl" style="color: #000000;"></i>';
+        minutesHand.style.backgroundColor = 'white';
+        hoursHand.style.backgroundColor = 'white';
     }else{
         document.body.classList = 'light';
         colorMode.innerHTML = '<i class="fa-solid fa-moon fa-2xl" style="color: #000000;"></i>';
+        minutesHand.style.backgroundColor = 'black';
+        hoursHand.style.backgroundColor = 'black';
     }
 })
 
 updateDateHour();
+const timeInterval = setInterval(updateDateHour, 1000)
